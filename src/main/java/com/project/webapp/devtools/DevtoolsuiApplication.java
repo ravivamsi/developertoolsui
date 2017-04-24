9a3ab7f20f9b33b91631692febd3a17e00dbb0f3
Vamsi Ravi <ivamsiravi@gmail.com>
2017-04-23 23:19:59 -0400

    Working on the UI of the Application

---
+++ src/main/java/com/project/webapp/devtools/DevtoolsuiApplication.java
@@ -0,0 +1,13 @@
+package com.project.webapp.devtools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevtoolsuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevtoolsuiApplication.class, args);
+	}
+}
+
---
+++ src/main/resources/static/app/homecontroller.js
@@ -0,0 +1,13 @@
+(function(){
+	'use strict';
+	
+	angular.module('app')
+	.controller('homecontroller', homecontroller);
+	
+	homecontroller.$inject = [];
+	
+	function homeRoot() {
+		var vm = this;
+	}
+	
+})();
